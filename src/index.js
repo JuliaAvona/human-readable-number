e1d58b833d1b15fd073b872e79d89c078ac89cf9
JuliaAvona <94717941+JuliaAvona@users.noreply.github.com>
2023-01-03 07:56:28 +0300

    init: Human Readable Number Solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,62 @@
-module.exports = function toReadable (number) {
-  
-}
+let simpleNum = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+];
+let dozens = [
+    'zero',
+    'ten',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+];
+
+
+
+module.exports = function toReadable(number) {
+    let numToStr = number + "";
+
+    function upToHundred(num) { // 0-99
+        let numStr = num + "";
+        if (num <= 19) {
+            return simpleNum[num];
+        } else if (num % 10 === 0) {
+            return dozens[numStr[0]];
+        } else  {
+            return `${dozens[numStr[0]]} ${simpleNum[numStr[1]]}`; // 20 - 99
+        }
+    }
+
+    if (numToStr.length < 3 ) { // 0-99
+        return upToHundred(number);
+    } else if (numToStr.length === 3 && number % 100 === 0) { // 100, 200, 300 .....
+        return `${simpleNum[numToStr[0]]} hundred`;
+    } else { // 100 - 999
+        let numToDozensPart = +(numToStr[1] + "" + numToStr[2]);
+        let dozensPart = upToHundred(numToDozensPart);
+        return `${simpleNum[numToStr[0]]} hundred ${dozensPart}`;
+    }
+}; 
+
